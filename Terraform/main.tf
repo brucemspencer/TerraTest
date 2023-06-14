5abe45c8e9f548783f652b7450543c17f70d4ef0
brucemspencer <100127864+brucemspencer@users.noreply.github.com>
2023-06-14 01:55:39 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,10 +4,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = example
-  location = East US
+  name     = "example"
+  location = "East US"
 }
 
-output "id {
+output "id" {
   value = data.azurerm_resource_group.example.id
 }
